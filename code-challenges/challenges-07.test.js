4d7f7fb549e6e7ee40f0c88cb01c05b5e352b986
Raghad <raghadmustafa96@yahoo.com>
2021-02-23 02:19:26 +0200

    code ch7

--- code-challenges/challenges-07.test.js
+++ code-challenges/challenges-07.test.js
@@ -8,6 +8,8 @@ Write a function named replaceZeros that, given a string, uses Regex to replace 
 
 const replaceZeros = (string) => {
   // Solution code here...
+  let num = string.replace(/0/g,'zero');
+    return num;
 }
 
 /* ------------------------------------------------------------------------------------------------
