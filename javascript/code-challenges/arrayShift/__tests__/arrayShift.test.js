0deec4b7e94c10f763bd5e59577b1d5aeb0329a8
Raghad <raghadmustafa96@yahoo.com>
2021-03-29 18:13:22 +0300

    add codeCallenge 2

--- javascript/code-challenges/arrayShift/__tests__/arrayShift.test.js
+++ javascript/code-challenges/arrayShift/__tests__/arrayShift.test.js
@@ -3,9 +3,16 @@
 const arrayShift = require('../array-shift.js');
 
 describe('arrayShift', () => {
-  it('should add the new number on the middle of array', () => {
+  it('should add the new number on the middle of array :)', () => {
     expect(arrayShift([1,2,3,4],5)).toStrictEqual([1,2,5,3,4]);
     expect(arrayShift([1,2,3,4,6],5)).toStrictEqual([1,2,3,5,4,6]);
+  });
+
+  it('Expected failure', () => {
+    expect(arrayShift([1,2,3,4,6],5)).not.toEqual([1,5,2,3,4,6]);
+  });
+
+  it('should the empty array return empty array', () => {
     expect(arrayShift([],5)).toStrictEqual([]);
   });
 });
